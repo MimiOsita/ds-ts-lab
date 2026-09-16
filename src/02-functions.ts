d00c8fa6a4d6feb4b282a20d93132b3047e0c24c
MimiOsita <20097663@mail.wit.ie>
2026-09-16 14:13:16 +0100

    Function to update friend's interests.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -67,3 +67,16 @@ function findFriends(
 
 console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
 console.log(findFriends(friends, (friend) => friend.age < 35));
+
+function addInterest(
+    f: Friend,
+    interest: string
+): string[] {
+    if (f.interests === undefined) {
+        f.interests = [];
+    }
+    f.interests.push(interest);
+    return f.interests;
+}
+
+console.log(addInterest(friends[1], 'Politics'));
