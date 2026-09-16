4bf933ddccea6680c12a8cdaa41e47ca240ba57c
MimiOsita <20097663@mail.wit.ie>
2026-09-16 12:48:45 +0100

    Higher-order functions.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,4 +1,4 @@
-import {Friend, Colleague } from './myTypes'
+import {Friend, Colleague, EmailContact } from './myTypes'
 import {friends, colleagues } from './01-basics'
 
 function older(f: Friend) : string {
@@ -8,7 +8,7 @@ function older(f: Friend) : string {
 
 console.log(older(friends[0]))
 
-function highestExtension(cs: Colleague[]): Colleague {
+function highestExtension(cs: Colleague[]) {
     const result = cs.sort(
         (c1, c2) => c1.contact.extension - c2.contact.extension
     );
@@ -26,10 +26,10 @@ function addColleague(
     const nextExtension = highest.contact.extension + 1;
 
     const newColleague: Colleague = {
-        name: name,
-        department: department,
+        name,
+        department,
         contact: {
-            email: email,
+            email,
             extension: nextExtension
         }
     };
@@ -38,3 +38,26 @@ function addColleague(
 
 addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
 console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
+
+function sortColleagues(
+    colleagues: Colleague[],
+    sorter: (c1: Colleague, c2: Colleague) => number
+): EmailContact[] {
+    const sorted = colleagues.sort(sorter);
+    const result: EmailContact[] = sorted.map((ce) => ({ name: ce.name, email: ce.contact.email}));
+    return result
+}
+
+console.log(sortColleagues(colleagues.current, (a, b) => a.contact.extension - b.contact.extension));
+console.log(sortColleagues(colleagues.current, (a, b) => a.name.length - b.name.length));
+
+function findFriends(
+    friendsArray: Friend[],
+    criterion: (f: Friend) => boolean
+): Friend[] {
+    const result = friendsArray.filter(criterion);
+    return result;
+}
+
+console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
+console.log(findFriends(friends, (friend) => friend.age < 35));
